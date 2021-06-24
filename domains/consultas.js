1acee39c873eea83e4b3ccd01213ecbe8f23182d
Leandro Chelentano <lchelentano@hotmail.com>
2021-06-24 20:14:02 -0300

    consultas html and toggle

---
+++ domains/consultas.js
@@ -0,0 +1,8 @@
+function close3() {
+    document.getElementById('transacciones').style.display = 'none'
+}
+
+function showTransacciones() {
+    toggleMenu();
+    document.getElementById('transacciones').style.display = 'block'
+}
